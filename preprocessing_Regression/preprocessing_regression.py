8720bdae45c68b62e6e78c7698f3a52689b8d4fe
Abanob Nabil <abanobnabil00000@gmail.com>
2026-08-05 19:53:12 +0300

    Add classification and regression preprocessing

--- preprocessing_Regression/preprocessing_regression.py
+++ preprocessing_Regression/preprocessing_regression.py
@@ -6,32 +6,51 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
-# Load Cleaned Dataset
 
-df = pd.read_csv("data/kc_house_cleaned.csv")
+def regression_preprocessing():
 
+    # -----------------------------
+    # Load Cleaned Dataset
+    # -----------------------------
+    df = pd.read_csv("data/kc_house_cleaned.csv")
 
-# Feature Selection
+    # -----------------------------
+    # Feature Selection
+    # -----------------------------
+    X = df.drop("price", axis=1)
+    y = df["price"]
 
-X = df.drop("price", axis=1)
-y = df["price"]
+    # -----------------------------
+    # Train Test Split
+    # -----------------------------
+    X_train, X_test, y_train, y_test = train_test_split(
+        X,
+        y,
+        test_size=0.2,
+        random_state=42
+    )
 
+    # -----------------------------
+    # Feature Scaling
+    # -----------------------------
+    scaler = StandardScaler()
 
-# Train Test Split
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X,
-    y,
-    test_size=0.2,
-    random_state=42
-)
-
-
-# Feature Scaling
-
-scaler = StandardScaler()
-
-X_train_scaled = scaler.fit_transform(X_train)
-X_test_scaled = scaler.transform(X_test)
-
+    # -----------------------------
+    # Return Results
+    # -----------------------------
+    return {
+        "df": df,
+        "X": X,
+        "y": y,
+        "X_train": X_train,
+        "X_test": X_test,
+        "y_train": y_train,
+        "y_test": y_test,
+        "X_train_scaled": X_train_scaled,
+        "X_test_scaled": X_test_scaled,
+        "scaler": scaler
+    }
 
